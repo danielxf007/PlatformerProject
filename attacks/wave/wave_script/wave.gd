63280664cc429b970232dd0b8c08657ac33a001e
Daniel Higuita <daniel.higuita007@gmail.com>
2019-01-27 17:08:48 -0500

    thrid change

---
+++ attacks/wave/wave_script/wave.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const GRAVITY = 1.0
+const HORIZONTAL_SPEED = 300.0
+const PUSH_FORCE = 500.0
+var direction
+var velocity = Vector2()
+
+func _ready():
+	set_as_toplevel(true)
+	velocity.x = direction.x * HORIZONTAL_SPEED
+
+func _physics_process(delta):
+	if is_outside_view_bounds():
+		queue_free()
+	velocity.y += GRAVITY * delta
+	var motion = velocity * delta
+	var collision_info = move_and_collide(motion)
+	if collision_info:
+		var collider = collision_info.collider
+		if collider.has_method("take_damage"):
+			collider.take_damage(2, Vector2(PUSH_FORCE, 0), 0.12, null)
+		queue_free()
+
+func is_outside_view_bounds():
+	var out_x_bound = position.x > OS.get_screen_size().x or position.x < 0.0
+	var out_y_bound = position.y > OS.get_screen_size().y or position.y < 0.0
+	return out_x_bound or out_y_bound
