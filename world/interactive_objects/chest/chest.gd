0670243df590a39df459c8900f39c3c1e12875cd
Daniel Higuita <daniel.higuita007@gmail.com>
2019-01-28 16:25:45 -0500

    save

---
+++ world/interactive_objects/chest/chest.gd
@@ -0,0 +1,18 @@
+extends Node2D
+export(int) var max_number_coins = 20
+var coins = 0.0
+
+func _ready():
+	coins = randi() % max_number_coins
+func _on_InteractiveArea_body_entered(body):
+	if body.name == "Player":
+		var purse = body.get_node("Purse")
+		purse.take_coin(coins)
+		$AnimationPlayer.play("dissapear")
+		$Timer.start()
+
+
+func _on_Timer_timeout():
+	$InteractiveArea/CollisionShape2D.disabled = true
+	queue_free()
+	
