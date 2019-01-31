3344c8fa3fdc008907e5904490e64026eee5b2ed
Daniel Higuita <daniel.higuita007@gmail.com>
2019-01-31 18:20:26 -0500

    change3

--- enemies/enemy_prototype/body_movement/states/motion/on_ground/move.gd
+++ enemies/enemy_prototype/body_movement/states/motion/on_ground/move.gd
@@ -9,7 +9,7 @@ func enter():
 func update(delta):
 	var input_direction = get_input_direction()
 	if input_direction == Vector2(0, 0):
-		emit_signal("finished", "idle")
+		emit_signal("finished", "Idle")
 	else:
 		update_look_direction(input_direction)
 		move(input_direction, delta)
@@ -19,7 +19,7 @@ func move(direction, delta):
 	velocity.y = GRAVITY
 	owner.move_and_slide(velocity, FLOOR_NORMAL)
 	if not owner.is_on_floor():
-		emit_signal("finished", "fall")
+		emit_signal("finished", "Fall")
 
 
 func handle_animation(ani_name):
